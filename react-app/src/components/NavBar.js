2533c3d4dc2843ea116150d559acc97837f6eef2
Jessie Baron <jessiebaron.political@gmail.com>
2022-11-24 10:25:03 -0500

    navbar changes

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,11 +2,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import './NavBar.css'
 
 const NavBar = () => {
   return (
     <nav>
-      <ul>
+      <div className="navigation-items">
+        <div className="fast-forward-logo">
+          <NavLink className="logo-container" to="/" exact={true}>
+            <i id='home-logo' class="fa-brands fa-tiktok"></i>
+            <div className="logo-text">FastForward</div>
+          </NavLink>
+        </div>
+        <div className='search-items'>
+          <input className='search-bar' type='search'>
+          </input>
+          <hr className="search-divider" />
+          <i id="search-icon" class="fa-solid fa-magnifying-glass"></i>
+        </div>
+        <div className='nav-buttons'>
+          <button className='upload-button'>+ Upload</button>
+          <button className='login-button'><NavLink className='login' to='/login' exact={true} activeClassName='active'>Login</NavLink></button>
+        </div>
+        {/* <ul>
         <li>
           <NavLink to='/' exact={true} activeClassName='active'>
             Home
@@ -30,7 +48,8 @@ const NavBar = () => {
         <li>
           <LogoutButton />
         </li>
-      </ul>
+      </ul> */}
+      </div>
     </nav>
   );
 }
