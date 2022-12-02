05d89eeb51fcd519f67e293df11a9f1e368037d3
Jessie Baron <jessiebaron.political@gmail.com>
2022-12-02 17:01:29 -0500

    finished validations

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -20,12 +20,17 @@ def username_exists(form, field):
     if user:
         raise ValidationError('Username is already in use.')
 
+def image_url_is_valid(form, field):
+    image_url = field.data
+    if not image_url.endswith('"pdf", "png", "jpg", "jpeg", "gif"'):
+        raise ValidationError('Please input a valid image URL')
+
 
 class SignUpForm(FlaskForm):
     first_name = StringField('firstName', validators=[DataRequired()])
     last_name = StringField('lastName', validators=[DataRequired()])
     bio = StringField('bio', validators=[DataRequired()])
-    image_url = StringField('imageUrl', validators=[DataRequired()])
+    image_url = StringField('imageUrl', validators=[DataRequired(), image_url_is_valid])
     username = StringField('username', validators=[DataRequired(), username_exists])
     email = EmailField('email', validators=[DataRequired(), Email(), user_exists])
     password = StringField('password', validators=[DataRequired()])
