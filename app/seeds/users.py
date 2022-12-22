e6062ec32823a7c98dc77419704a614b4dc86a0a
Jessie Baron <jessiebaron.political@gmail.com>
2022-12-22 11:04:02 -0500

    small fixes

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -10,15 +10,15 @@ def seed_users():
     bobbie = User(
         first_name='Bobbie', last_name='Smiths', image_url='https://static-cse.canva.com/blob/951359/1600w-YTfEMXMuMCs.jpg', bio='Creator from South Georgia! Roll Tide!', username='bobbie', email='bobbie@aa.io', password='password')
     Bella = User(
-        first_name='Bella', last_name='Poarch', image_url='https://p16-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/673f78c28ed852aa8c3d97c43d8f1694~c5_720x720.jpeg?x-expires=1671552000&x-signature=HqTgubjS56CAYkMSitYD75LLeF4%3D', bio='No bio yet.', username='bellapoarch', email='bella@aa.io', password='password')
+        first_name='Bella', last_name='Poarch', image_url='https://editors.dexerto.com/wp-content/uploads/2021/01/BellaPoarchTop5.jpg', bio='No bio yet.', username='bellapoarch', email='bella@aa.io', password='password')
     Will = User(
-        first_name='Will', last_name='Smith', image_url='https://p16-sign-va.tiktokcdn.com/musically-maliva-obj/1646315618666501~c5_720x720.jpeg?x-expires=1671483600&x-signature=GOpjYuwOMLsJjoIx2R1YBnsM3YE%3D', username='willsmith', email='will@aa.io', password='password')
+        first_name='Will', last_name='Smith', image_url='https://media.wired.com/photos/5d960eba01e4a4000826137c/master/pass/Culture_Monitor_WillSmith-465783654.jpg', username='willsmith', email='will@aa.io', password='password')
     Zach = User(
-        first_name='Zach', last_name='King', image_url='https://p16-sign-sg.tiktokcdn.com/aweme/720x720/tos-alisg-avt-0068/smgf5f369c884044a8df770614bbfd64717.jpeg?x-expires=1671523200&x-signature=nYC1HQm1liOVEHiDPXot36CLOSc%3D', username='zachking', email='zach@aa.io', password='password')
+        first_name='Zach', last_name='King', image_url='https://i.guim.co.uk/img/media/4ea36f9ca7d8900fde5957a15d64ecef7180364b/0_1343_4480_2688/master/4480.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=f90a12b25e5d0f30ccbbce7431422d57', username='zachking', email='zach@aa.io', password='password')
     Khaby = User(
         first_name='Khabane', last_name='Lame', image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDOE1dBmDsBMGigsXAgt8Udrc4e7oKJoik6A&usqp=CAU', username='khaby.lame', email='khaby@aa.io', password='password')
     Rock = User(
-        first_name='The', last_name='Rock', image_url='https://p16-sign-va.tiktokcdn.com/musically-maliva-obj/1647596478025734~c5_720x720.jpeg?x-expires=1671552000&x-signature=%2B2sWvQjpy32Df1t6%2F9RDxPUrMSQ%3D', username='therock', email='rock@aa.io', password='password')
+        first_name='The', last_name='Rock', image_url='https://parade.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTk0NDIzNDY0NzQyODg4OTY1/black-adam-madrid-premiere.jpg', username='therock', email='rock@aa.io', password='password')
 
 
 
